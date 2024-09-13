3c768f5da2bb6f322d19af9b951938d8cb4582b1
Rishi Banerjee <rishieric91@gmail.com>
2024-09-14 01:57:01 +0530

    auto_route added

---
+++ lib/routing/routing.dart
@@ -0,0 +1,18 @@
+// Package imports:
+import 'package:auto_route/auto_route.dart';
+
+// Project imports:
+import 'package:words625/routing/routing.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.cupertino();
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: SplashRoute.page, initial: true),
+        AutoRoute(page: LoginRoute.page),
+        AutoRoute(page: LangChoiceRoute.page),
+        AutoRoute(page: HomeRoute.page),
+      ];
+}
