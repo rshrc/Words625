2a739e0b5519451f79225ef10332d2d6c70f5f4c
Rishi Banerjee <rishieric91@gmail.com>
2024-10-19 13:31:26 +0530

    implement getFormattedTime function to improve time formatting

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -19,3 +19,9 @@ Map<K, V> shuffleMap<K, V>(Map<K, V> inputMap) {
 
   return Map.fromEntries(entries);
 }
+
+String getFormattedTime(int totalSeconds) {
+  final minutes = (totalSeconds ~/ 60).toString().padLeft(2, '0');
+  final seconds = (totalSeconds % 60).toString().padLeft(2, '0');
+  return "$minutes:$seconds";
+}
