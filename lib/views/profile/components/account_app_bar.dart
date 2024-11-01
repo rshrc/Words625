5bc2692b39deb4ed01b5786c51dd635aaf1ab0dc
Rishi Banerjee <rishieric91@gmail.com>
2024-11-01 11:44:46 +0530

    Refactor AccountAppBar to use StreamingSharedPreferences and add Avatar and AccountWidget components

--- lib/views/profile/components/account_app_bar.dart
+++ lib/views/profile/components/account_app_bar.dart
@@ -1,6 +1,15 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
+
+// Project imports:
+import 'package:words625/di/injection.dart';
+import 'package:words625/domain/auth/firebase_user.dart';
+import 'package:words625/gen/assets.gen.dart';
+import 'package:words625/service/locator.dart';
+
 class AccountAppBar extends StatelessWidget implements PreferredSizeWidget {
   const AccountAppBar({Key? key}) : super(key: key);
 
@@ -9,58 +18,75 @@ class AccountAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      toolbarHeight: 120,
-      backgroundColor: Colors.white,
-      elevation: 1.5,
-      leading: account(),
-      leadingWidth: 100,
-      centerTitle: false,
-      actions: [
-        avatar(),
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        AccountWidget(),
+        Avatar(),
       ],
     );
   }
+}
 
-  Widget avatar() {
+class Avatar extends StatelessWidget {
+  const Avatar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
-      child: const CircleAvatar(
+      child: CircleAvatar(
         backgroundColor: Colors.transparent,
-        backgroundImage: AssetImage('assets/images/profile.jpg'),
-        radius: 37,
+        backgroundImage: AssetImage(
+          Assets.images.profilePng.path,
+        ),
+        radius: 24,
       ),
     );
   }
+}
 
-  Widget account() {
+class AccountWidget extends StatelessWidget {
+  const AccountWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(left: 10, top: 15),
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Rishi',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF4B4B4B),
+      child: PreferenceBuilder<SerializableFirebaseUser?>(
+        preference: getIt<AppPrefs>().authUser,
+        builder: (BuildContext context, SerializableFirebaseUser? user) {
+          // Default values if user is null
+          final displayName = user?.displayName ?? 'Guest';
+          final email = user?.email ?? 'No email';
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  displayName,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4B4B4B),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'rshrc',
-              style: TextStyle(
-                fontSize: 15,
-                color: Color(0xFFAFAFAF),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  email,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    color: Color(0xFFAFAFAF),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
