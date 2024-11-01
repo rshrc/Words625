e7e586cf77abb3f688bc166a16f5605fac24295b
Rishi Banerjee <rishieric91@gmail.com>
2024-11-01 11:45:21 +0530

    Refactor SplashPage to check authentication status on initialization and navigate to HomeRoute if user is authenticated

--- lib/views/splash/splash_page.dart
+++ lib/views/splash/splash_page.dart
@@ -2,12 +2,14 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 
 // Project imports:
+import 'package:words625/di/injection.dart';
+import 'package:words625/routing/routing.gr.dart';
+import 'package:words625/service/locator.dart';
 import 'components/center_display.dart';
 import 'components/get_started_button.dart';
-import 'components/log_in_button.dart';
 
 @RoutePage()
 class SplashPage extends StatefulWidget {
@@ -21,29 +23,37 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    super.initState();
+    _checkAuthStatus();
+  }
+
+  Future<void> _checkAuthStatus() async {
+    final user = getIt<AppPrefs>().authUser.getValue();
+
+    if (user != null) {
+      context.router.replace(const HomeRoute());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             const Expanded(child: CenterDisplay()),
-            bottomButtons(),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GetStartedButton(context),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
           ],
         ),
-      ),
-    );
-  }
-
-  bottomButtons() {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GetStartedButton(context),
-          const SizedBox(height: 8),
-          LogInButton(context),
-          const SizedBox(height: 16),
-        ],
       ),
     );
   }
