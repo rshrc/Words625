054f7d256f03040b20be4dcda3f2c30aa7f0b239
Rishi Banerjee <rishieric91@gmail.com>
2024-11-06 17:28:14 +0530

    make LeaderboardPage constant in home page

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
     const CourseTree(),
     const CharacterPracticeScreen(),
     const ProfilePage(),
-    LeaderboardPage(),
+    const LeaderboardPage(),
     const ShopPage(),
     // const ExplorePage(),
   ];
