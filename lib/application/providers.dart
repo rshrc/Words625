86cd372ede3e1630edbe687033f7df52b878b081
Rishi Banerjee <rishieric91@gmail.com>
2024-11-16 16:05:32 +0530

    Add providers for LanguageProvider, CourseProvider, and GameProvider

--- lib/application/providers.dart
+++ lib/application/providers.dart
@@ -3,6 +3,9 @@ import 'package:provider/provider.dart';
 
 // Project imports:
 import 'package:words625/application/character_provider.dart';
+import 'package:words625/application/course_provider.dart';
+import 'package:words625/application/game_provider.dart';
+import 'package:words625/application/language_provider.dart';
 import 'package:words625/application/level_provider.dart';
 import 'package:words625/di/injection.dart';
 
@@ -12,5 +15,15 @@ final providers = [
   ),
   ChangeNotifierProvider<CharacterProvider>(
     create: (_) => getIt<CharacterProvider>(),
+  ),
+  ChangeNotifierProvider<LanguageProvider>(
+    create: (_) => getIt<LanguageProvider>(),
+  ),
+  ChangeNotifierProvider<CourseProvider>(
+    create: (_) => getIt<CourseProvider>(),
+  ),
+  // for GameProvider
+  ChangeNotifierProvider<GameProvider>(
+    create: (_) => getIt<GameProvider>(),
   ),
 ];
